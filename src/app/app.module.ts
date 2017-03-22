653129f82d869102800bf5a2fbdf151de907bd6a
john greer <johngreer1978@gmail.com>
2017-03-22 15:45:47 +0000

    part 1/2 of angular2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { MycomponentComponent } from './mycomponent/mycomponent.component';
+import {AF} from './providers/af';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { Routes, RouterModule} from '@angular/router';
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyB55exQWgqr9RPR2unRv-gFqKhUZCRVdXc',
+  authDomain: 'testapp-68044.firebaseapp.com',
+  databaseURL: 'https://testapp-68044.firebaseio.com',
+  storageBucket: 'testapp-68044.appspot.com',
+  messagingSenderId: '812861050432'
+
+};
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'login', component: LoginPageComponent }
+];
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    AngularFireModule.initializeApp(firebaseConfig),
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent, MycomponentComponent, LoginPageComponent, HomePageComponent ],
+  bootstrap: [ AppComponent ],
+  providers: [AF]
 })
-export class AppModule { }
+export class AppModule {}
+
+
